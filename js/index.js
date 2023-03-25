b8ba6e7d3fac9f1a9d4f31d4255f7a673dd52e50
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-03-26 01:06:32 +0900

    헤더 및 메인비주얼 섹션 추가

--- js/index.js
+++ js/index.js
@@ -2,4 +2,25 @@
  * 풀페이지 (Pageable)
  * https://github.com/Mobius1/Pageable#browser
  */
-new Pageable("#container");
+new Pageable("#container", {
+  events: {
+    keydown: false, // enable / disable keyboard navigation
+  },
+});
+
+// /**
+//  * 한글자씩 타이핑
+//  */
+// let titleContent = "안녕하세요! \n 웹퍼블리셔 김다희 입니다. :)";
+// let typing = document.querySelector(".home__title");
+// let i = 0;
+
+// function typingText() {
+//   if (i < titleContent.length) {
+//     let txt = titleContent.charAt(i);
+//     typing.innerHTML += txt == "\n" ? "<br/>" : txt;
+//     i++;
+//   }
+// }
+
+// setInterval(typingText, 50);
