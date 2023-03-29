05b8d65de35c5c0cdd78add23dc700f284b59162
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-03-30 00:10:05 +0900

    풀페이지 애니메이션 효과 적용

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
-/**
- * 터치 기기 & 모바일 사이즈 풀페이지 예외처리
- */
 // 터치 기기 여부
 const isTouchDevice = window.matchMedia("(pointer: coarse)").matches;
 // 모바일 사이즈 여부
 const isMobile = window.innerWidth < 768;
 
-if (isTouchDevice || isMobile) {
+/**
+ * PC사이즈 or 터치 기기일 경우만 풀페이지 적용
+ */
+if (!isTouchDevice || !isMobile) {
+  /**
+   * 풀페이지 (Pageable)
+   * https://github.com/Mobius1/Pageable
+   */
+  const pages = new Pageable("#container", {
+    pips: true, // 우측 네비게이션 여부
+    animation: 450, //스크롤 애니메이션의 지속 시간
+  });
+
+  // 풀페이지 애니메이션 효과 적용
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach((section) => {
+    sections[0].classList.add("animated");
+    //스크롤 시작시 애니메이션 클래스명 추가/제거
+    pages.on("scroll.start", (data) => {
+      section.classList.remove("animated");
+      sections[data.index].classList.add("animated");
+    });
+  });
+
+  document.querySelector("#container").classList.remove("not-pageable");
+} else {
   // 모바일 사이즈 or 터치 가능 기기일 경우
   document.querySelector("#container").classList.add("not-pageable");
-} else {
-  // 풀페이지 (Pageable)
-  new Pageable("#container", {
-    pips: true,
-    animation: 450,
-    swipeThreshold: 1000,
-  });
-  document.querySelector("#container").classList.remove("not-pageable");
 }
