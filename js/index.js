2528272647fef6981e56ca181b41c399b29f0fad
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-03-29 17:32:40 +0900

    모바일 사이즈 반응형 작업

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
 /**
- * 풀페이지 (Pageable)
- * https://github.com/Mobius1/Pageable#browser
+ * 터치 기기 & 모바일 사이즈 풀페이지 예외처리
  */
-const pages = new Pageable("#container", {
-  pips: true,
-  animation: 450,
-  events: {
-    keydown: true, // enable / disable keyboard navigation
-  },
-});
+// 터치 기기 여부
+const isTouchDevice = window.matchMedia("(pointer: coarse)").matches;
+// 모바일 사이즈 여부
+const isMobile = window.innerWidth < 768;
+
+if (isTouchDevice || isMobile) {
+  // 모바일 사이즈 or 터치 가능 기기일 경우
+  document.querySelector("#container").classList.add("not-pageable");
+} else {
+  // 풀페이지 (Pageable)
+  new Pageable("#container", {
+    pips: true,
+    animation: 450,
+    swipeThreshold: 1000,
+  });
+  document.querySelector("#container").classList.remove("not-pageable");
+}
